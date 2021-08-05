1ab5d7fee6e292afc374732718577789b3e84b2b
ssafyWoo <zbxv1423@naver.com>
2021-08-05 18:00:52 +0900

    add empty contents page to all needed pages

--- frontend/src/views/Mobile/profile/UserMychelin.vue
+++ frontend/src/views/Mobile/profile/UserMychelin.vue
@@ -1,5 +1,8 @@
 <template>
     <div>
+        <div v-if="isEmpty">
+            <EmptyContent data="맛집 리스트" />
+        </div>
         <div class="row shadow p-3 mb-3 select-box" v-for="my in mychelin" :key="my.id">
             <div v-on:click="clickMychelinDetail(my.id)">{{ my.title }}</div>
         </div>
@@ -7,11 +10,19 @@
 </template>
 
 <script>
+import EmptyContent from '@/components/error/EmptyContent'
 export default {
+    components: {
+        EmptyContent
+    },
     props: {
         mychelin: Array
     },
-    
+    computed: {
+        isEmpty() {
+            return this.mychelin.length === 0
+        }
+    }    
 }
 </script>
 
