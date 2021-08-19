3308ef23f5997aa5f74b2693a6cc9bada8e8af6d
WoosubLeee <zbxv1423@naver.com>
2021-08-19 22:35:45 +0900

    mfti result 2

--- frontend/src/views/user/mfti/MftiResult.vue
+++ frontend/src/views/user/mfti/MftiResult.vue
@@ -3,10 +3,15 @@
         <MainNavbar/>
         <div class="result-wrap">
             <div class="mfti-description">
-                <p>당신은 <span>{{ result.placePreference.userAsAction }}</span></p>
-                <p><span>{{ result.placePreference.userAsAnimal }}</span>입니다</p>
+                <p>당신은 <span>{{ result.userAsAnimal }}</span>입니다</p>
             </div>
             <canvas id="myChart" width="500" height="500"></canvas>
+            <div v-for="(type, idx) in characters" :key="idx" class="type-wrap">
+                <span :class="type.result ? 'unselected' : 'selected'">{{ type.options[0] }}</span>
+                <span>vs</span>
+                <span :class="type.result ? 'selected' : 'unselected'">{{ type.options[1] }}</span>
+            </div>
+            {{ result.placePreference.explanation }}
         </div>
     </div>
 </template>
@@ -27,12 +32,41 @@ export default {
             },
             resultLabels: ['단 음식', '짠 음식', '신 음식', '기름진 음식', '매운 음식'],
             resultDatas: [],
+            characters: {
+                challenging: {
+                    options: ['모 험', '안 정'],
+                    result: 0,
+                },
+                planning: {
+                    options: ['계 획', '즉 흥'],
+                    result: 0,
+                },
+                sociable: {
+                    options: ['외 향', '내 향'],
+                    result: 0,
+                }, 
+                sensitivity: {
+                    options: ['민 감', '무 던'],
+                    result: 0,
+                },
+            },
         }
+    },
+    computed: {
+        characterResult() {
+            console.log(this.result.placePreference)
+            return this.result.placePreference.map
+        },
     },
     watch: {
         result() {
             this.resultDatas = Object.values(this.result.tastePreference)
             this.createChart()
+
+            const types = Object.keys(this.characters)
+            for (let i=0; i<4; i++) {
+                this.characters[types[i]].result = this.result.placePreference[types[i]]
+            }
         }
     },
     created() {
@@ -93,15 +127,34 @@ export default {
 
 <style scoped>
 .result-wrap {
-    margin: 100px 20px 0;
+    margin: 80px 20px 0;
 }
 .mfti-description {
     text-align: center;
     font-size: 18px;
-    margin: 20px;
+    margin: 10px 0 0;
 }
 .mfti-description span {
     font-size: 28px;
     font-weight: 800;
+}
+.type-wrap {
+    text-align: center;
+    font-size: 18px;
+    margin: 5px 0;
+}
+.type-wrap > span {
+    display: inline-block;
+    margin: 0 15px;
+    width: 50px;
+}
+.selected {
+    font-weight: bold;
+    font-size: 20px;
+}
+.unselected {
+    font-weight: normal;
+    color: #9b9b9b;
+    font-size: 16px;
 }
 </style>
